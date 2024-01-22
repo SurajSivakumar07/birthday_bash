e211fc8ef83cba470447e8a538eb869347ab2be3
Suraj.S <surajsuri279@gmail.com>
2024-01-22 15:26:19 +0530

    Day 1 has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hello_World from './components/Hello_World'
 
 export default function App() {
   return (
     <>
-
-      <h1>hello </h1>
+ 
+      <Hello_World />
+     
       
     </>
   )
