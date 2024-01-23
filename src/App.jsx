b4b48a340d8306739cc2dfecc1b0b46ed8a3767f
Suraj.S <surajsuri279@gmail.com>
2024-01-23 15:34:19 +0530

    Day2 has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Hello_World from './components/Hello_World'
+import { Routes ,Route, Link} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
 
 export default function App() {
   return (
     <>
+      
+        <NavBar />
+      <Routes>
+
+        
+
+      </Routes>
  
-      <Hello_World />
-     
       
     </>
   )
